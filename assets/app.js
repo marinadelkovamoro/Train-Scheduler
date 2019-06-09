7427f588b006f2dfd8246883ec5776dc234513a0
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-06-08 17:25:26 -0700

    created Firebase event for adding trains to the database and a row in the html each time a user adds an entry

--- assets/app.js
+++ assets/app.js
@@ -17,7 +17,7 @@ firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
 // When the user submits the inputs, the 
-$("#add-train-btn").on("click", function(event) {
+$("#add-train-btn").on("click", function (event) {
   event.preventDefault();
 
   // Grabs user input
@@ -38,7 +38,7 @@ $("#add-train-btn").on("click", function(event) {
   // Uploads employee data to the database
   database.ref().push(newTrain);
 
-  // Logs everything to console
+  // Logs everything that was input to the console
   console.log(newTrain.name);
   console.log(newTrain.destination);
   console.log(newTrain.firstTrain);
@@ -50,5 +50,34 @@ $("#add-train-btn").on("click", function(event) {
   $("#InputFirstTrainTime").val("");
   $("#InputFrequency").val("");
 
+});
+
+// Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function (childSnapshot) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().destination;
+  var firstTrainTime = childSnapshot.val().firstTrain;
+  var frequency = childSnapshot.val().frequency;
+
+  // Log train Info
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+
+  // Create the new row for each train added
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(destination),
+    $("<td>").text(firstTrainTime),
+    $("<td>").text(frequency)
+  );
+
+  // Append the new row to the table
+  $(".table > tbody").append(newRow);
 });
 
